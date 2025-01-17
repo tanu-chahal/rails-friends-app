08334d456ae42a4779fdecdb6ee9666ca0237c67
Tanu Chahal <tanuchahal0004@gmail.com>
2025-01-18 00:29:57 +0530

    Done with authentication, associations and design.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :friends
-  root 'home#index'
+  # root 'home#index'
+  root 'friends#index'
   get 'home/about'
   # get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
